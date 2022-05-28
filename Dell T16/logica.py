d4493399189c782a0146cef7497ca25399a28e8d
leonardoteixeira23 <leo.dst23@gmail.com>
2022-05-28 14:49:59 -0300

    .

--- Dell T16/logica.py
+++ Dell T16/logica.py
@@ -8,9 +8,9 @@ class logica(object):
     def buscaPorNome(self,entrada):
         df2020 = self.df.loc[self.df['COMERCIALIZAÇÃO 2020'] == 'Sim']
         dfVendi2020 = pd.DataFrame()
+        index = 0
         for i in df2020['Subistancia']:
-            print(i)
             if entrada in i:
-                print(i)
-                dfVendi2020 += i
+                dfVendi2020 = pd.concat([dfVendi2020,df2020], ignore_index=True, axis=1)
+            index += 1
         print(dfVendi2020)
