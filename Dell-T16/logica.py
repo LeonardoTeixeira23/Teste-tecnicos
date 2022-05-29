be2e3777394265150e13a232bd773cfb5f65d04e
leonardoteixeira23 <leo.dst23@gmail.com>
2022-05-29 19:10:05 -0300

    5

--- Dell-T16/logica.py
+++ Dell-T16/logica.py
@@ -2,22 +2,23 @@ import pandas as pd
 import unidecode as uni
 
 
+
 class logica(object):
 
     def __init__(self):
         
-        caminho_tabela = '/home/leonardo/Workspace/Teste-tecnicos/Dell T16/TA_PRECO_MEDICAMENTO.csv'
-
+        caminho_tabela = 'TA_PRECO_MEDICAMENTO.csv'
+      
         table = pd.read_csv(caminho_tabela, delimiter = ';', encoding='unicode_escape', low_memory=False)
         self.df = pd.DataFrame(table)
     
-    #Cria um dataframe só com os items comercializados em 2020
+    #Cria um DataFrame só com os items comercializados em 2020
     def df_2020(self):
         df_2020 = self.df.loc[self.df['COMERCIALIZAÇÃO 2020'] == 'Sim'].reset_index()
         return df_2020
     
     def busca_por_nome(self,entrada):
-        #Cria um dataframe só com as colunas necesseraias
+        #Cria um DataFrame só com as colunas necesseraias
         df_2020 = self.df_2020()
         df_2020 = df_2020[['SUBSTÂNCIA', 'PRODUTO','APRESENTAÇÃO', 'PF Sem Impostos']]
       
@@ -26,7 +27,7 @@ class logica(object):
         entrada = entrada.upper()
         
         
-        #Cria um novo dataframe apenas com os itens correspondentes a pesquisa
+        #Cria um novo DataFrame apenas com os itens correspondentes a pesquisa
         index = 0
         df_2020_por_nome = pd.DataFrame()      
         for i in df_2020['SUBSTÂNCIA']:
@@ -37,7 +38,7 @@ class logica(object):
                 df_2020_por_nome = pd.concat([df_2020_por_nome,df_aux], ignore_index=True)
             index+=1
 
-        #testa se o a corespondencia do item pesquisado no df
+        #Testa se o a corespondencia do item pesquisado no DataFrame
         if df_2020_por_nome.empty == True:
             print('Produto não encotrado.\n')
         else: 
@@ -46,7 +47,7 @@ class logica(object):
     def busca_cod_barras(self, entrada):
 
         try:
-            #Cria um novo dataframe apenas com os itens correspondentes a pesquisa
+            #Cria um novo DataFrame apenas com os itens correspondentes a pesquisa
             prod = self.df.loc[self.df['EAN 1'] == entrada, 'PRODUTO'].item()
             df_prod = self.df.loc[self.df['PRODUTO'] == prod]
         
@@ -58,10 +59,10 @@ class logica(object):
             min = df_pmc.min()
             max = df_pmc.max()
         except:
-            #Cria uma df fazio para o tratamendo de erro
+            #Cria uma DataFrame vazio para o tratamendo de erro
             df_prod = pd.DataFrame()
 
-        #testa se o a corespondencia do item pesquisado no df
+        #Testa se o a corespondencia do item pesquisado no DataFrame
         if df_prod.empty == True:
             print('\nCodigo de barras não encotrado')
         else:
@@ -75,13 +76,14 @@ class logica(object):
         #Captura quantos itens a no "df_2020"
         total_itens = len(df_2020)      
 
-        #Caputara a porcentagem de ocorrencia de cada um dos status no df
+        #Caputara a porcentagem de ocorrencia de cada um dos status no DataFrame
         negativo = (len(df_2020[df_2020['LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO (PIS/COFINS)'] == 'Negativa'])/total_itens)*100
         neutro = (len(df_2020[df_2020['LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO (PIS/COFINS)'] == 'Neutra'])/total_itens)*100
         positivo = (len(df_2020[df_2020['LISTA DE CONCESSÃO DE CRÉDITO TRIBUTÁRIO (PIS/COFINS)'] == 'Positiva'])/total_itens)*100
         
         print(f'\nCLASSIFICACAO   PERCENTUAL    GRAFICO\n')
-        print(f'Negativa         %.2f%s       %s'%((negativo,'%','*'*int(negativo))))
+        print(f'Negativa         %.2f%s       %s' %((negativo,'%','*'*int(negativo))))
         print(f'Neutra           %.2f%s       %s' %(neutro,'%','*'*int(neutro)))
-        print(f'Positiva         %.2f%s       %s\n' %(positivo,'%','*'*int(positivo)))
+        print(f'Positiva         %.2f%s       %s' %(positivo,'%','*'*int(positivo)))
+        print(f'Total            %.2f%s\n' %((negativo+neutro+positivo),'%'))
         
